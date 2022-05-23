7c16234ae931ea9808cc9e5abf7bf825b12573df
Benny Nazimov <66024037+benny-n@users.noreply.github.com>
2022-05-23 20:25:30 +0300

    replibyte 1.3.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/replibyte.rb
+++ Formula/replibyte.rb
@@ -1,9 +1,9 @@
 class Replibyte < Formula
-  version "1.3.2"
+  version "1.3.3"
   desc "Seed Your Development Database With Real Data ⚡️"
   homepage "https://github.com/Qovery/replibyte"
-  url "https://github.com/benny-n/replibyte/releases/download/v1.3.2/replibyte_v1.3.2_x86_64-apple-darwin.zip"
-  sha256 "3a956ff66cd57ebc01a4ae70bab02fb598b8081de383ea5841f32056cf34cc64"
+  url "https://github.com/benny-n/replibyte/releases/download/v1.3.3/replibyte_v1.3.3_x86_64-apple-darwin.zip"
+  sha256 "b3cc4fb566af8d69348962f01c37bbdaeefec715406373275721976292237973"
   license "MIT"
 
   def install
